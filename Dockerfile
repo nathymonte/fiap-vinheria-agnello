260f2b3879ef02f400a2cba48fbccb7f7f153fae
Nicolas Caous Fernandes <caousn@hotmail.com>
2025-03-29 15:58:51 -0300

    Fazendo projeto rodar sem eclipse

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM tomcat:11-jdk21
+COPY target/vag-1.0.war /usr/local/tomcat/webapps/
---
+++ src/main/java/br/edu/fiap/ideaseeders/App.java
@@ -0,0 +1,10 @@
+package br.edu.fiap.ideaseeders;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
